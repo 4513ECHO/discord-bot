27107b65d89a38b304706d678c6e6296360e74c4
響 <81011153+4513ECHO@users.noreply.github.com>
2021-07-03 16:43:30 +0900

    Update testcog.py

--- cogs/testcog.py
+++ cogs/testcog.py
@@ -14,18 +14,17 @@ class TestCog(commands.Cog):
     async def neko(self, ctx):
         await ctx.send("にゃーん")
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        msg = message.content
-        if message.author.bot:
-            return
-
-        for x in MESSAGES.keys():
-            if re.match(x, msg):
-                await message.channel.send(MESSAGES[x])
-                return
-        await message.channel.send("そうだね")
+#    @commands.Cog.listener()
+#    async def on_message(self, message):
+#        msg = message.content
+#        if message.author.bot:
+#            return
+#
+#        for x in MESSAGES.keys():
+#            if re.match(x, msg):
+#                await message.channel.send(MESSAGES[x])
+#                return
+#        await message.channel.send("そうだね")
 
 def setup(bot):
     bot.add_cog(TestCog(bot))
-
