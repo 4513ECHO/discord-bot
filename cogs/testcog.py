35d812c5bed613f3aa7726f9a4e7c4b2b278d4ba
4513ECHO <4513ECHO@gmail.com>
2021-07-03 07:15:13 +0000

    [update] add cog

--- cogs/testcog.py
+++ cogs/testcog.py
@@ -1,4 +1,10 @@
 from discord.ext import commands
+import re
+
+MESSAGES = {}
+with open("messages.txt", 'r') as f:
+    for x in f:
+        MESSAGES[x.split()[0]] = x.split()[1]
 
 class TestCog(commands.Cog):
     def __init__(self, bot):
@@ -7,6 +13,18 @@ class TestCog(commands.Cog):
     @commands.command()
     async def neko(self, ctx):
         await ctx.send("にゃーん")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        msg = message.content
+        if message.author.bot:
+            return
+
+        for x in MESSAGES.keys():
+            if re.match(x, msg.strip(PREFIX)):
+                await message.channel.send(MESSAGES[x])
+                return
+        await message.channel.send("そうだね")
 
 def setup(bot):
     bot.add_cog(TestCog(bot))
--- discordbot.py
+++ discordbot.py
@@ -11,21 +11,6 @@ INITAL_EXTENSIONS = [
 
 PREFIX = "#"
 
-MESSAGES = {r"neko": "にゃーん",
-            r"(死|し)にたい": "いきててえらいよ",
-            r"やったぜ": "よかったね",
-            r"かわいい.*\?$": "もちろんそうだよ",
-            r"かわいい": "ありがとう",
-            r"病んだ": "げんきあげる",
-            r"ありがとう": "こちらこそ",
-            r"(かなしい|悲しい|つらい|辛い|)": "はなしきくよ",
-            r"(つかれた|疲れた)": "ゆっくり休んで いきててえらいね",
-            r"^ねえねえ": "なあに",
-            r"こんにちは": "こんにちは",
-            r"おはよう": "おはよう",
-            r"おやすみ": "おやすみ"
-           }
-
 class MyBot(commands.Bot):
     def __init__(self, command_prefix):
         super().__init__(command_prefix)
@@ -41,19 +26,6 @@ class MyBot(commands.Bot):
         print(self.user.name)
         print(self.user.id)
         print("-----")
-
-
-@client.event
-async def on_message(message):
-    msg = message.content
-    if message.author.bot or not msg.startswith(PREFIX):
-        return
-
-    for x in MESSAGES.keys():
-        if re.match(x, msg.strip(PREFIX)):
-            await message.channel.send(MESSAGES[x])
-            return
-    await message.channel.send("そうだね")
 
 if __name__ == '__main__':
     bot = MyBot(command_prefix=PREFIX)
